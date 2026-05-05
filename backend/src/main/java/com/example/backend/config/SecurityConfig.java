f2dbed74ae44f4ac09a25960c84f8a51fae3d69d
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2026-05-05 22:02:04 +0200

    Backend Init

---
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
+        return http.build();
+    }
+
+}
